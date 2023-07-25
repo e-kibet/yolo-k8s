06ce484ee129aed5cec6d52f508f229820dfae35
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2023-07-25 23:52:57 +0300

    set up the dockerfile and docker compose yml file

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,17 @@ let mongodb_url = 'mongodb://localhost/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ekibet:ekibet@172.20.0.2:27017'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 let db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
+db.once('open', () => {
     console.log('Database connected successfully')
 })
 
 // Check for DB Errors
-db.on('error', (error)=>{
+db.on('error', (error) => {
     console.log(error);
 })
 
@@ -32,7 +32,7 @@ const app = express()
 app.use(express.json())
 
 // 
-app.use(upload.array()); 
+app.use(upload.array());
 
 // Cors 
 app.use(cors());
@@ -43,6 +43,6 @@ app.use('/api/products', productRoute)
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
